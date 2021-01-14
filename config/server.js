c6655e687cd9a2cf70e08cc3bae873f31aa2d80f
David Lam <davidlam@Davids-MacBook-Pro.local>
2021-01-14 16:56:02 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2346a73703a9cf399d8e72bde0dcccae'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a851f721-ccf4-4b56-8720-57b181b3603d'
+};
